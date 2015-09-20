117d0500d207b2cb30e054693957b7858855bf61
Satrya <margasatrya@gmail.com>
2015-09-20 19:50:30 +0700

    Version 1.0.0

---
+++ includes/functions.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Various functions used by the plugin.
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Sets up the default arguments.
+ */
+function cwp_get_default_args() {
+
+	$defaults = array(
+		'title'         => esc_attr__( 'Recent Comments', 'comments-widget-plus' ),
+		'title_url'     => '',
+		'post_type'     => 'post',
+		'limit'         => 5,
+		'offset'        => '',
+		'order'         => 'DESC',
+		'avatar'        => 0,
+		'avatar_size'   => 55,
+		'excerpt'       => 0,
+		'excerpt_limit' => 50,
+		'css_class'     => '',
+	);
+
+	// Allow plugins/themes developer to filter the default arguments.
+	return apply_filters( 'cwp_default_args', $defaults );
+
+}
+
+/**
+ * Generates the recent comments markup.
+ */
+function cwp_get_recent_comments( $args = array() ) {
+
+	// Set up a default, empty variable.
+	$html = '';
+
+	// Merge the input arguments and the defaults.
+	$args = wp_parse_args( $args, cwp_get_default_args() );
+
+	// Extract the array to allow easy use of variables.
+	extract( $args );
+
+	// Allow devs to hook in stuff before the recent comments.
+	do_action( 'cwp_before_loop' );
+
+	// Recent comments query.
+	$comments = cwp_get_comments( $args );
+
+	if ( is_array( $comments ) && $comments ) :
+
+		$html = '<ul class="cwp-ul ' . ( ! empty( $args['css_class'] ) ? '' . sanitize_html_class( $args['css_class'] ) . '' : '' ) . '">';
+
+			foreach( $comments as $comment ) :
+
+				$html .= '<li class="recentcomments cwp-li">';
+
+					if ( $args['avatar'] ) :
+						$html .= '<a class="comment-link cwp-comment-link" href="' . esc_url( get_comment_link( $comment->comment_ID ) ) . '">';
+							$html .= '<span class="comment-avatar cwp-avatar">' . get_avatar( $comment->comment_author_email, $args['avatar_size'] ) . '</span>';
+						$html .= '</a>';
+					endif;
+
+					$html .= '<span class="cwp-comment-title">';
+						/* translators: comments widget: 1: comment author, 2: post link */
+						$html .= sprintf( _x( '%1$s on %2$s', 'widgets', 'comments-widget-plus' ),
+							'<span class="comment-author-link cwp-author-link">' . get_comment_author_link( $comment->comment_ID ) . '</span>',
+							'<a class="comment-link cwp-comment-link" href="' . esc_url( get_comment_link( $comment->comment_ID ) ) . '">' . get_the_title( $comment->comment_post_ID ) . '</a>'
+						);
+					$html .= '</span>';
+
+					if ( $args['excerpt'] ) :
+						$html .= '<span class="comment-excerpt cwp-comment-excerpt">' . wp_html_excerpt( $comment->comment_content, $args['excerpt_limit'], '&hellip;' ) . '</span>';
+					endif;
+
+				$html .= '</li>';
+
+			endforeach;
+
+		$html .= '</ul>';
+
+	endif;
+
+	// Allow devs to hook in stuff after the loop.
+	do_action( 'cwp_after_loop' );
+
+	// Return the  posts markup.
+	return $html;
+
+}
+
+/**
+ * The recent comments query.
+ */
+function cwp_get_comments( $args = array() ) {
+
+	// Arguments
+	$query = array( 
+		'number'      => $args['limit'],
+		'offset'      => $args['offset'],
+		'order'       => $args['order'],
+		'post_status' => 'publish',
+		'post_type'   => $args['post_type'],
+		'status'      => 'approve',
+	);
+
+	// Allow plugins/themes developer to filter the default query.
+	$query = apply_filters( 'cwp_default_query_args', $query );
+
+	// Get the comments.
+	$comments = get_comments( $query );
+
+	return $comments;
+
+}
---
+++ readme.txt
@@ -0,0 +1,75 @@
+=== Comments Widget Plus ===
+Contributors: satrya, themephe
+Tags: recent comments, widget, recent comments widget, excerpt, avatar
+Requires at least: 4.0
+Tested up to: 4.3.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Provides custom recent comments widget with extra features such as display avatar, comment excerpt and much more!
+
+== Description ==
+
+This plugin will enable a custom and advanced recent comments widget. Allows you to display a list of the most recent comments with avatar and excerpt, you can also choose which to show newer comments first or older comments first and choose comments from any post type.
+
+= Features Include =
+
+* Display avatar with customizable size.
+* Display comment excerpt with customizable length.
+* Allows you to set title url.
+* Custom CSS class.
+* Post type option.
+* Offset option.
+* Option to choose the comments order.
+* Multiple widgets.
+
+= Support =
+
+* [Forum support](http://wordpress.org/support/plugin/comments-widget-plus).
+* [Rate/Review the plugin](http://wordpress.org/support/view/plugin-reviews/comments-widget-plus).
+* [Submit translation](https://github.com/themephe/comments-widget-plus/issues).
+* [Contribute on Github](https://github.com/themephe/comments-widget-plus/)
+
+> Developed by [ThemePhe](https://themephe.com/) in Indonesia
+
+== Installation ==
+
+**Through Dashboard**
+
+1. Log in to your WordPress admin panel and go to Plugins -> Add New
+2. Type **comments widget plus** in the search box and click on search button.
+3. Find **Comments Widget Plus** plugin.
+4. Then click on Install Now after that activate the plugin.
+5. Go to the widgets page **Appearance -> Widgets**.
+6. Find **Recent Comments Plus** widget.
+
+**Installing Via FTP**
+
+1. Download the plugin to your hardisk.
+2. Unzip.
+3. Upload the **comments-widget-plus** folder into your plugins directory.
+4. Log in to your WordPress admin panel and click the Plugins menu.
+5. Then activate the plugin.
+6. Go to the widgets page **Appearance -> Widgets**.
+6. Find **Recent Comments Plus** widget.
+
+== Frequently Asked Questions ==
+
+= How to disable default style =
+This plugin add a small css code to your website, to remove it please add the code below in your theme `functions.php`
+`
+add_filter( 'cwp_use_default_style', '__return_false' );
+`
+
+== Screenshots ==
+
+1. General settings
+2. Comments settings
+3. Avatar settings
+4. Excerpt settings
+
+== Changelog ==
+
+= 1.0.0 - Sept 20, 2015 = 
+- Initial release
